8f0b5c2f0631e2c5913267ac38ca351afc5aa54e
Carlos Cobos <info@kcobos.es>
2019-05-21 23:20:13 +0200

    Ideas

--- ad4l.py
+++ ad4l.py
@@ -1,19 +1,53 @@
 #!/usr/bin/python3
 import subprocess
-
-procesos = {}
+import json
 
 def top():
+    procesos = {}
     cabeceras = ["pid", "user", "pr", "ni", "virt", "res", "shr", "s" "cpu", "mem", "time"]
     top = subprocess.run(['top', '-n 1', '-b'], stdout=subprocess.PIPE)
     lineas = top.stdout.split(b"\n")
     for linea in lineas[7:]:
         campos = linea.split()
-        campos = list(map(lambda x: x.decode('ascii'), campos))
-        if campos[11] not in procesos:
+        if len(campos) == 12:
+            campos = list(map(lambda x: x.decode('ascii'), campos))
             valor = [[c] for c in campos[0:10]]
-            procesos[campos[11]] = dict(zip(cabeceras, valor))
-        print(procesos)
-        exit()
+            dicValor = dict(zip(cabeceras, valor))
+            if campos[11] not in procesos:
+                procesos[campos[11]] = dicValor
+            else:
+                for c in cabeceras:
+                    procesos[campos[11]][c] = procesos[campos[11]][c] + dicValor[c]
+            # print(procesos)
+            # exit()
+    return procesos
 
-top()
+procesos_actuales = top()
+print(json.dumps(procesos_actuales, sort_keys=True, indent=4))
+
+import psutil
+
+PROCNAME = "firefox"
+p = None
+for proc in psutil.process_iter():
+    if proc.name() == PROCNAME:
+        print(proc)
+        p = proc
+
+p.cpu_percent()
+p.memory_full_info()
+p.io_counters()
+psutil.net_io_counters()
+
+
+import subprocess
+df = subprocess.check_output("lsusb")
+df
+# split por \n
+
+#dict de procesos 
+# historico cpu, mem, io
+
+# historico de net
+
+# alerta cuando algo cambia drásticamente sin sentido notify-send "hola cara bola"
